53d43838e38a39f9caf3ac4f3cfea028eef50674
hedqvistpersson <124053990+hedqvistpersson@users.noreply.github.com>
2024-04-25 14:40:54 +0200

    mitt jobb

---
+++ lib/test.py
@@ -0,0 +1,5 @@
+import random
+
+def get_sisters():
+    sisters = random.randint(1,5)
+    return sisters
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from lib.test import get_sisters
+
+app = Flask(__name__) 
+
+@app.route('/')
+def index():   
+    sisters = get_sisters()
+    return render_template('index.html', test=str(sisters))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
